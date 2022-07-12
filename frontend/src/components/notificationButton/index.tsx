30d3023ced4ae398544a4893a696003b00eeb428
Luiz Felipe <felipe.marques_80@hotmail.com>
2022-07-12 15:52:28 -0300

    useState

--- frontend/src/components/notificationButton/index.tsx
+++ frontend/src/components/notificationButton/index.tsx
@@ -1,6 +1,5 @@
-import App from "../../App"
-import icon from '../../assets/img/notification-icon.svg'
-import './styles.css'
+import icon from '../../assets/img/notification-icon.svg';
+import './styles.css';
 function NotificationButton() {
     return (
         <div className="dsmeta-red-btn">
@@ -9,4 +8,4 @@ function NotificationButton() {
     )
 }
 
-export default NotificationButton
+export default NotificationButton;
